b4266f50c99313025a47b6a9ebbcd33ecf0f97c3
EthanBrayZA <bra23004@byui.edu>
2023-09-21 16:59:11 +0200

    w02-task

---
+++ scripts/w02-task.js
@@ -0,0 +1,33 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+const fullName = 'Ethan Bray';
+let currentYear = '2023';
+let profilePicture = images/portrait.jpg;
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('src');
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${currentYear}`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`); 
+            
+
+
+/* Step 5 - Array */
+let favFoods = ['Venison', 'Lasagna','Pizza','Ice Cream'];
+foodElement.innerHTML = favFoods.join('<br>');
+const newFavFood = 'Chocolate';
+favFoods.push(newFavFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift();
+foodElement.innerHTML += `<br>${favFoods.join('<br>')}`;
+favFoods.pop();
+foodElement.innerHTML += `<br>${favFoods.join('<br>')}`;
